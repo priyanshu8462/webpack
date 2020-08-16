d4e42aca29446745a45d9f032f761daac5e5886b
priyanshu yadav <priyanshu20202.gmail.com>
2020-08-16 23:32:40 +0530

    second-commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import _ from "lodash";
+import "./style.css";
+import image from "./photo.jpg";
+import Data from "./data.xml";
 function Component(){
    const element = document.createElement('h1');
 
    element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
+   element.classList.add('heading');
+   const myImage = new Image();
+   element.appendChild(myImage)
+   console.log(Data);
    return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,44 @@ const path =require('path');
 module.exports ={
    entry : "./src/index.js",
    output : {
-      filename : "main.js",
+      filename : "bundle.js",
       path : path.resolve(__dirname,'dist')
+   },
+   mode : "development",
+   module : {
+      rules : [
+         // for css files
+         {
+            test :/\.css$/,
+            use :[
+               "style-loader",
+               "css-loader"
+            ]
+         },
+         // for font files
+         {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: [
+               'file-loader',
+               ],
+         },
+         // for images, svg, gif
+         {
+            test :  /\.(png|jpg|svg|gif)$/,
+            use : [
+               "file-loader"
+            ]
+         },
+         //for csv
+         {
+            test : /\.(csv|tsv)$/,
+            use : [ "csv-loader" ]
+         },
+         //for xml
+         {
+            test : /\.xml$/,
+            use :["xml-loader"]
+         }
+      ]
    }
 };
